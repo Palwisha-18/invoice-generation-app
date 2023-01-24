207fd0023043f39a3bfcdeb8a840ce2822d45c22
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2023-01-23 21:57:23 -0700

    Add table from excel to PDF

--- main.py
+++ main.py
@@ -9,17 +9,36 @@ filepaths = glob.glob("invoices/*.xlsx")
 
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     filename = Path(filepath).stem
     invoice_num, invoice_date = filename.split('-')
 
-    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_num}", align="L", ln=1, border=0)
 
-    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=f"Date: {invoice_date}", align="L", ln=1, border=0)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    columns = [item.replace('_', ' ').title() for item in df.columns]
+
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
 
     pdf.output(f"PDFs/{filename}.pdf")
